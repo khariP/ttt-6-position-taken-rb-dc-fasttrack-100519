0cf8c6b8f15c048741112b197aec27e3c1d1a86f
super-communicator-6104 <kpierce.tto@gmail.com>
2019-10-05 19:32:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,6 @@
-# code your #position_taken? method here!
+# code your #position_taken? method here!def position_taken?
+  #return tru or false depending
+end
+
+
+game_board + ["X", "", "0", "", "", "" , ]
